2eb56ed1d0dd29cd85b1d48a3a0b3a40ad5bf3a8
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-17 19:12:53 +0530

    Correct typos in functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -21,15 +21,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox,get_attribute('placeholder'), 
+        self.assertEqual(inputbox.get_attribute('placeholder'), 
                 'Enter a to-do item')
 
         # Edith types "But peacock feathers: into a text box.
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # 1: Buy peacockfeathers" as an item.in to-do list
         inputbox.send_keys(Keys.ENTER)
@@ -38,11 +37,13 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1:Buy peacock feathers' for row in rows)
+                any(row.text == '1:Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
                 )
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
