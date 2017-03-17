434dd43313d1d34826014e2ea6268b593dfb181a
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-17 17:15:16 +0530

    Functional test check we can input a to-do item

--- functinal_tests.py
+++ functinal_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -11,19 +13,33 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes to
-        # check check out its homepage
+        # Edith has heard about a cool new online to-do app. 
+        #  She goes to check check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox,get_attribute('placeholder'), 
+                'Enter a to-do item')
 
         # Edith types "But peacock feathers: into a text box.
+        input.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates, and now the page lists
         # 1: Buy peacockfeathers" as an item.in to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1:Buy peacock feathers' for row in rows)
+                )
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -39,3 +55,4 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
+
