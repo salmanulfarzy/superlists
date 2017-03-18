ada298d6f803db6f7b11abcda44281d165738942
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-19 00:23:15 +0530

    Test list data submission

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from lists.models import Item
 
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_next', '')
+    item.save()
+
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
