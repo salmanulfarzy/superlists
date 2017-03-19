29013efd0660b8e247808410e51c6b786fbaf10e
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-19 19:39:13 +0530

    Add URL, view and test for new list
    - URL for new list
    - move new list  test to separate class with URL
    -  new view for list items

--- lists/views.py
+++ lists/views.py
@@ -3,10 +3,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 def view_list(request):
@@ -14,4 +10,8 @@ def view_list(request):
 
     context = {'items': items}
     return render(request, 'list.html', context)
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
