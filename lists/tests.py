a872de0634cfba27488ec941c91732eabc5cbf0d
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-19 20:48:25 +0530

    Associate each item with a list using FK

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -23,16 +23,24 @@ class NewListTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -41,8 +49,10 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text,
                 'The first list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text,
                 'The second item')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -52,8 +62,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text = 'Item 1')
-        Item.objects.create(text = 'Item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text = 'Item 1', list=list_)
+        Item.objects.create(text = 'Item 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
