f1fe30f26e93e25112d6d998e18a93c6efdc1a84
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-19 18:57:35 +0530

    Sepated templated for input and listing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -90,7 +90,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
-        page_text = self.browse.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
