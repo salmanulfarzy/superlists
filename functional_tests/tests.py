b952b56f6c4d2788653ed5456469a3971a5bbcb2
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-24 15:06:18 +0530

    Add check for test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,8 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+import sys
+import os
 import time
 import unittest
 
@@ -12,6 +14,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            setattr(self, 'live_server_url', 'http://' + staging_server)
 
     def tearDown(self):
         self.browser.quit()
