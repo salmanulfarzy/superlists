3489f80db0a1bb478b6eee1964e4042f55a4be97
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-03-29 12:55:21 +0530

    fabric didn't work with python3.6

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,7 +7,7 @@ REPO_URL = 'https://github.com/sfarzy/superlists.git'
 
 def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
-    source_folder = site_folder = '/source'
+    source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
@@ -16,7 +16,7 @@ def deploy():
     _update_database(source_folder)
 
 def _create_directory_structure_if_necessary(site_folder):
-    for subfolder uin ('database', 'static', 'virtualenv', 'source'):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run(f'mkdir -p {site_folder}/{subfolder}')
 
 def _get_latest_source(source_folder):
@@ -24,7 +24,7 @@ def _get_latest_source(source_folder):
         run(f'cd {source_folder} && git fetch')
     else:
         run(f'git clone {REPO_URL} {source_folder}')
-    current_commit =local("git log -n 1 --format=%H", capture=True)
+    current_commit = local("git log -n 1 --format=%H", capture=True)
     run(f'cd {source_folder} && git reset --hard {current_commit}')
 
 def _update_settings(source_folder, site_name):
@@ -37,7 +37,7 @@ def _update_settings(source_folder, site_name):
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        keys = ''.join(random.SystemRandom().chioce(chars) for _ in range(50))
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
         append(secret_key_file, f'SECRET_KEY = "{key}"')
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
@@ -50,12 +50,12 @@ def _update_virtualenv(source_folder):
 def _update_static_files(source_folder):
     run(
             f'cd {source_folder}'
-            '&& ../virtualenv/bin/python manage.py collectstatic --noinput'
+            ' && ../virtualenv/bin/python manage.py collectstatic --noinput'
             )
 
 def _update_database(source_folder):
     run(
             f'cd {source_folder}'
-            '&& ../virtualenv/bin/python manage.py migrate --noinput'
+            ' && ../virtualenv/bin/python manage.py migrate --noinput'
             )
 
