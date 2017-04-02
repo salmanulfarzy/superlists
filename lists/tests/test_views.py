6f57828343b738de559a3b841c014377f0e64863
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-02 12:32:27 +0530

    Refactot list item to handle new item POSTs
    Use the same view to process the POST requests as to render the form
    that they come from. This is has an important advantage that same URL
    can display a form, and display ant errors encountered in processing the
    users's input.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -51,7 +51,7 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
-    def test_displays_all_items(self):
+    def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text = 'Item 1', list=correct_list)
         Item.objects.create(text = 'Item 2', list=correct_list)
@@ -68,15 +68,12 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'Other Item 1')
         self.assertNotContains(response, 'Other Item 2')
 
-
-class NewItemTest(TestCase):
-
-    def test_can_save_a_POST_request_to_an_exisitng_list(self):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-                f'/lists/{correct_list.id}/add_item',
+                f'/lists/{correct_list.id}/',
                 data={'item_text': 'A new item for an existing list'}
                 )
 
@@ -85,12 +82,12 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-                f'/lists/{correct_list.id}/add_item',
+                f'/lists/{correct_list.id}/',
                 data={'item_text': 'A new item for an existing list'}
                 )
 
