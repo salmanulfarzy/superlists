7244ce653dc06aae371cb8408eb28babed6b66f6
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-04 20:21:49 +0530

    FT to clear validation error message on input

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,9 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         #  Edith goes to the home page and accidentally tries to submit
         #  an emoty list ite. She hits Enter on the empty input box
@@ -59,7 +62,28 @@ class ItemValidationTest(FunctionalTest):
 
         #  She sees a helpful message
         self.wait_for(lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector('.has-error').text,
+                self.get_error_element().text,
                 "You've already got this in your list"
                 ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        #  Edith start a list and causes a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Hodor')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Hodor')
+        self.get_item_input_box().send_keys('Hodor')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+            ))
+
+        #  She starts typing in the input box box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        #  She is pleased to see that the errror message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+            ))
 
