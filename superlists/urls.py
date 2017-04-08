012670f344cdf333d85c027f99023c556ea78906
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-08 14:59:13 +0530

    Add URLs for accounts, login, send_login_email

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from lists import views as list_views
 from lists import urls as list_urls
-
+from accounts import urls as account_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(account_urls)),
 ]
 
