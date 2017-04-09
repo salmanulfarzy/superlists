796025f8b6b0b192b34e9800f7aa3fe66b2c61c6
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-09 12:22:49 +0530

    Refactor FT base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,9 +2,24 @@ import os
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10
+
+
+def wait(fn):
+        def modified_fn(*args, **kwargs):
+            start_time = time.time()
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (AssertionError, WebDriverException) as e:
+                    if time.time() - start_time > MAX_WAIT:
+                        raise e
+                    time.sleep(0.5)
+        return modified_fn
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -17,21 +32,6 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
-
     @wait
     def wait_for(self, fn):
         return fn()
@@ -41,6 +41,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
 
     @wait
     def wait_to_be_logged_in(self, email):
