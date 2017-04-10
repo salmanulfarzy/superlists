74a8f510fdb9dbaa12a40348b1cd75c1a165995e
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-10 17:51:47 +0530

    Move session creation from my_lists to base FT

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,10 +1,14 @@
 import os
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
+User = get_user_model()
 MAX_WAIT = 10
 
 
@@ -54,6 +58,21 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table(
                 '{}: {}'.format(item_number, item_text)
                 )
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+
+        #  Visit the domain to set a cookie. 404 pages load faster
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+            ))
 
     @wait
     def wait_to_be_logged_in(self, email):
