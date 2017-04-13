6e9c0cdaaf1ddc4e33df18ecc1083094631396e5
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-13 20:30:27 +0530

    Initial heroku configuration

--- superlists/settings.py
+++ superlists/settings.py
@@ -3,10 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!o!9pnlqan4b+4v_l(s26wdvnx7d9%-32+)1x*y7*8aa=zrkl)'
 
@@ -115,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'sfarzy67@gmail.com'
@@ -139,3 +135,24 @@ LOGGING = {
         'root': { 'level': 'INFO' },
         }
 
+# Heroku Settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+            'default': dj_database_url.config(default='postgres://localhost')
+            }
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X-FORWARDED_PROTO', 'https')
+
+    # Allow all host header
+    ALLOWED_HOSTS = ['*']
+    DEBUG = False
+
+    # Static access configuration
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = {
+            os.path.join(BASE_DIR, 'static')
+    }
