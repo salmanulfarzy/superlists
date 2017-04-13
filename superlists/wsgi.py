0c2b09ee58c058786573e3779c82234a78fcc8b7
Salmanul Farzy <salmanulfarzy@gmail.com>
2017-04-13 22:05:09 +0530

    Use whitenoise to server static files in heroku

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for superlists project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
